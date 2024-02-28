f4bb84dfea6b439d54ffec311b7cf98df4ea9948
Tbryant7 <tannonbryant@gmail.com>
2024-02-28 16:10:36 -0500

    Added ASCII for dog

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,3 +1,9 @@
+/*Tannon Bryant
+ * 2/28/24
+ * Animal Picker
+ */
+
+
 import java.util.Scanner;
 
 public class AnimalPicker {
@@ -5,11 +11,21 @@ public class AnimalPicker {
         Scanner scanner = new Scanner(System.in);
         String choice;
 
-        // Continually ask the user for a valid option
+        // Asking the user for what animal they would like to select
         while (true) {
             System.out.println("Which animal would you like to see: a dog, a cat, or a fish?");
             System.out.print("Enter your choice: ");
             choice = scanner.nextLine().toLowerCase();
+            // Checking what the user selected
+            if (choice.equals("dog")) {
+                // ASCII art for dog
+                System.out.println("              __");
+                System.out.println(" (\\,--------'()'--o");
+                System.out.println("  (_    ___    /~\"");
+                System.out.println("   (_)_)  (_)_)");
+            
+                break;
+            } 
 
             
         }
