1275eb141933b276d1ec2c0dec96a2672e17273d
Tbryant7 <tannonbryant@gmail.com>
2024-02-28 16:03:22 -0500

    added while loop and entry message

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,5 +1,19 @@
-/*
- * Tannon Bryant
- * 2/28/24
- * AnimalPicker
- */
+import java.util.Scanner;
+
+public class AnimalPicker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice;
+
+        // Continually ask the user for a valid option
+        while (true) {
+            System.out.println("Which animal would you like to see: a dog, a cat, or a fish?");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextLine().toLowerCase();
+
+            
+        }
+
+
+    }
+}
